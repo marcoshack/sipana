ca846c61155b0b82f6714c1db013e5f279303b41
marcoshack <marcoshack@1070b9c0-c02c-0410-8e95-a1ee7272c91a>
2008-02-09 12:34:26 +0000

    - SIPScenario refactory. Now on its own project sipana-sipscenario - Method SIPSessionLogic.show() implemented to generate the SIP scenario

--- sipana-commons/src/main/java/org/sipana/protocol/sip/impl/SIPFactoryImpl.java
+++ sipana-commons/src/main/java/org/sipana/protocol/sip/impl/SIPFactoryImpl.java
@@ -8,18 +8,17 @@ import java.text.ParseException;
 import javax.sip.address.SipURI;
 
 import org.apache.log4j.Logger;
-import org.sipana.protocol.sip.SIPMessage;
 import org.sipana.protocol.sip.SIPFactory;
+import org.sipana.protocol.sip.SIPMessage;
 import org.sipana.protocol.sip.SIPRequest;
 import org.sipana.protocol.sip.SIPResponse;
 import org.sipana.protocol.sip.SIPSession;
-
 
 public class SIPFactoryImpl implements SIPFactory, ParseExceptionListener {
     private static SIPFactory instance;
     private StringMsgParser parser = new StringMsgParser();
     private Logger logger;
-    
+
     private SIPFactoryImpl() {
         parser = new StringMsgParser(this);
         logger = Logger.getLogger(SIPFactory.class);
@@ -27,27 +26,32 @@ public class SIPFactoryImpl implements SIPFactory, ParseExceptionListener {
 
     public SIPMessage createMessage(byte[] rawMessage) throws ParseException {
         gov.nist.javax.sip.message.SIPMessage sipMessage = parser.parseSIPMessage(rawMessage);
+        return createMessage(sipMessage);
+    }
+
+    public SIPMessage createMessage(
+            gov.nist.javax.sip.message.SIPMessage sipMessage) {
         SIPMessage message = null;
-        
+
         if (sipMessage instanceof gov.nist.javax.sip.message.SIPRequest) {
             message = new SIPRequestImpl();
-            gov.nist.javax.sip.message.SIPRequest sipRequest = (gov.nist.javax.sip.message.SIPRequest)sipMessage;
-            SIPRequest request = (SIPRequest)message;
-            
+            gov.nist.javax.sip.message.SIPRequest sipRequest = (gov.nist.javax.sip.message.SIPRequest) sipMessage;
+            SIPRequest request = (SIPRequest) message;
+
             request.setMethod(sipRequest.getMethod());
             request.setMaxForwards(sipRequest.getMaxForwards().getMaxForwards());
-            
+
             // Request address host:port
-            SipURI requestURI = (SipURI)sipRequest.getRequestLine().getUri();
+            SipURI requestURI = (SipURI) sipRequest.getRequestLine().getUri();
             StringBuilder sbAddr = new StringBuilder(requestURI.getHost());
             sbAddr.append(":").append(requestURI.getPort());
             request.setRequestAddr(sbAddr.toString());
-            
+
         } else if (sipMessage instanceof gov.nist.javax.sip.message.SIPResponse) {
             message = new SIPResponseImpl();
-            gov.nist.javax.sip.message.SIPResponse sipResponse = (gov.nist.javax.sip.message.SIPResponse)sipMessage;
-            SIPResponse response = (SIPResponse)message;
-            
+            gov.nist.javax.sip.message.SIPResponse sipResponse = (gov.nist.javax.sip.message.SIPResponse) sipMessage;
+            SIPResponse response = (SIPResponse) message;
+
             response.setStatusCode(sipResponse.getStatusCode());
             response.setReasonPhrase(sipResponse.getReasonPhrase());
             response.setRelatedRequestMethod(sipResponse.getCSeq().getMethod());
@@ -55,22 +59,24 @@ public class SIPFactoryImpl implements SIPFactory, ParseExceptionListener {
 
         if (message != null) {
             message.setCallID(sipMessage.getCallId().getCallId());
-            
+
             // From user@domain
-            SipURI fromURI = (SipURI) sipMessage.getFrom().getAddress().getURI();
+            SipURI fromURI = (SipURI) sipMessage.getFrom()
+                    .getAddress()
+                    .getURI();
             message.setFromUser(getUser(fromURI));
-            
+
             // To user@domain
             SipURI toURI = (SipURI) sipMessage.getTo().getAddress().getURI();
             message.setToUser(getUser(toURI));
-            
+
         } else {
             logger.warn("Message info is null");
         }
 
         return message;
     }
-    
+
     private String getUser(SipURI uri) {
         StringBuilder sbUser = new StringBuilder(uri.getUser());
         sbUser.append("@").append(uri.getHost());
@@ -84,10 +90,9 @@ public class SIPFactoryImpl implements SIPFactory, ParseExceptionListener {
         return instance;
     }
 
-    public void handleException(ParseException ex, 
+    public void handleException(ParseException ex,
             gov.nist.javax.sip.message.SIPMessage sipMessage,
-            Class headerClass, String headerText, String messageText)
-    {
+            Class headerClass, String headerText, String messageText) {
         try {
             if (sipMessage != null && sipMessage.getCallId() != null) {
                 String call_id = sipMessage.getCallId().getCallId();
@@ -103,11 +108,12 @@ public class SIPFactoryImpl implements SIPFactory, ParseExceptionListener {
                 logger.warn(sb, ex);
             }
         } catch (Throwable t) {
-            logger.error("Fail handling NIST JAIN-SIP exception: " + t.getMessage(), t);
+            logger.error("Fail handling NIST JAIN-SIP exception: "
+                    + t.getMessage(), t);
         }
     }
 
-	public SIPSession createSession(SIPRequest request) {
-		return new SIPSessionImpl(request);
-	}
+    public SIPSession createSession(SIPRequest request) {
+        return new SIPSessionImpl(request);
+    }
 }
--- sipana-server-war/src/main/java/org/sipana/server/web/SIPSessionLogic.java
+++ sipana-server-war/src/main/java/org/sipana/server/web/SIPSessionLogic.java
@@ -1,7 +1,10 @@
 package org.sipana.server.web;
 
+import java.io.OutputStream;
 import java.util.Calendar;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.sipana.protocol.sip.SIPMessage;
 import org.sipana.protocol.sip.impl.SIPSessionImpl;
@@ -9,44 +12,53 @@ import org.sipana.server.service.Service;
 import org.sipana.server.service.ServiceLocator;
 import org.sipana.server.service.ServiceLocatorException;
 import org.sipana.server.sip.SIPSessionManager;
+import org.sipana.sip.scenario.SIPScenario;
 import org.vraptor.annotations.Component;
+import org.vraptor.annotations.In;
 import org.vraptor.annotations.Out;
 import org.vraptor.annotations.Parameter;
 
 @Component
 public class SIPSessionLogic {
-	
-	@Out
-	private List<SIPSessionImpl> sipSessions = null;
-	
-	@Out
-	private List<SIPMessage> sipMessages = null;
-	
-	@Parameter
-	private long id = 0;
-	
-	@Parameter
-	private String call_id = null;
-	
-	public void list() throws Exception {
-		this.sipSessions = getSIPSessionManager().getSIPSessions(0, Calendar.getInstance().getTimeInMillis());
-	}
-	
-	public void show() throws Exception {
-		if (id != 0) {
-			this.sipMessages = getSIPSessionManager().getMessageListBySessionId(id);
-		} else if (call_id != null) {
-			processCallIDList(call_id);
-		}
-	}
-	
-	private void processCallIDList(String list) throws ServiceLocatorException {
-		String items[] = list.split(",");
-		sipMessages = getSIPSessionManager().getMessageListByCallID(items);
-	}
 
-	private SIPSessionManager getSIPSessionManager() throws ServiceLocatorException {
-		ServiceLocator serviceLocator = ServiceLocator.getInstance();
-		return (SIPSessionManager) serviceLocator.getService(Service.SIP_SESSION_MANAGER);
-	}
+    @In
+    private HttpServletResponse response;
+
+    @Out
+    private List<SIPSessionImpl> sipSessions = null;
+
+    @Out
+    private List<SIPMessage> sipMessages = null;
+
+    @Parameter
+    private long id = 0;
+
+    @Parameter
+    private String call_id_list = null;
+
+    private SIPSessionManager sipSessionManager;
+
+    SIPSessionLogic() throws ServiceLocatorException {
+        ServiceLocator serviceLocator = ServiceLocator.getInstance();
+        sipSessionManager = (SIPSessionManager) serviceLocator.getService(Service.SIP_SESSION_MANAGER);
+    }
+
+    public void list() throws Exception {
+        long timeNow = Calendar.getInstance().getTimeInMillis();
+        this.sipSessions = sipSessionManager.getSIPSessions(0, timeNow);
+    }
+
+    public void show() throws Exception {
+        if (id != 0) {
+            sipMessages = sipSessionManager.getMessageListBySessionId(id);
+        } else if (call_id_list != null) {
+            String items[] = call_id_list.split(",");
+            sipMessages = sipSessionManager.getMessageListByCallID(items);
+        }
+
+        if (sipMessages != null) {
+            OutputStream outputStream = response.getOutputStream();
+            SIPScenario.createSIPScenario(sipMessages, outputStream);
+        }
+    }
 }
