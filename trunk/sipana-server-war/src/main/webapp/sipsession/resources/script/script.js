10933cceae7489ef245b1c5c873b9371837b2cc3
dcesar <dcesar@1070b9c0-c02c-0410-8e95-a1ee7272c91a>
2008-03-28 14:13:48 +0000

    added search filter hours and minutes

--- trunk/sipana-server-war/src/main/webapp/sipsession/resources/script/script.js
+++ trunk/sipana-server-war/src/main/webapp/sipsession/resources/script/script.js
@@ -4,16 +4,20 @@ input = document.getElementsByTagName("input");
 
 Calendar.setup({
         inputField     :    input[0],
-        ifFormat       :    "%d/%m/%Y",
+        ifFormat       :    "%d/%m/%Y %H:%M",
         button         :    "trigger_start",
         align          :    "Tl",
+        showsTime      :    true,
+        timeFormat     :    "24",
         singleClick    :    true
 });   
 Calendar.setup({
         inputField     :    input[1],
-        ifFormat       :    "%d/%m/%Y",
+        ifFormat       :    "%d/%m/%Y %H:%M",
         button         :    "trigger_end",
         align          :    "Tl",
+        showsTime      :    true,
+        timeFormat     :    "24",
         singleClick    :    true
 });
 
