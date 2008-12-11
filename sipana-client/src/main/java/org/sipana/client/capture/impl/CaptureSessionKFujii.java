97ac8e100d42780ba1312a74fdcd730fa2b55638
marcoshack <marcoshack@1070b9c0-c02c-0410-8e95-a1ee7272c91a>
2008-12-11 21:19:23 +0000

    Change jpcap library to Keita Fujii implementation [1].
    [1] http://netresearch.ics.uci.edu/kfujii/jpcap/doc/

---
+++ sipana-client/src/main/java/org/sipana/client/capture/impl/CaptureSessionKFujii.java
@@ -0,0 +1,112 @@
+package org.sipana.client.capture.impl;
+
+import java.util.Date;
+
+import jpcap.JpcapCaptor;
+import jpcap.NetworkInterface;
+import jpcap.PacketReceiver;
+import jpcap.packet.IPPacket;
+import jpcap.packet.TCPPacket;
+import jpcap.packet.UDPPacket;
+
+import org.sipana.client.capture.AbstractCaptureSession;
+import org.sipana.client.capture.CaptureException;
+import org.sipana.client.capture.CaptureListener;
+import org.sipana.client.capture.Packet;
+
+public class CaptureSessionKFujii extends AbstractCaptureSession implements
+        PacketReceiver {
+
+    private static int DEFAULT_SNAPLEN = 1500;
+    private static int DEFAULT_TIMEOUT = 0;
+    private static int DEFAULT_COUNT = -1; // Infinite
+
+    private JpcapCaptor captor;
+
+    public CaptureSessionKFujii(String filter, String deviceName,
+            CaptureListener listener) {
+        super(filter, deviceName, listener);
+    }
+
+    @Override
+    protected void startCapture() throws CaptureException {
+        try {
+            NetworkInterface[] interfaces = JpcapCaptor.getDeviceList();
+            NetworkInterface selectedIface = null;
+            StringBuilder sbIfList = new StringBuilder("Available network interfaces: ");
+
+            for (NetworkInterface iface : interfaces) {
+                sbIfList.append(iface.name).append(", ");
+
+                if (iface.name.equals(getDevice())) {
+                    selectedIface = iface;
+                }
+            }
+
+            if (logger.isDebugEnabled()) {
+                logger.debug(sbIfList);
+                StringBuilder sb = new StringBuilder("Selected network interface: ");
+                sb.append(selectedIface.name);
+                logger.debug(sb);
+            }
+
+            captor = JpcapCaptor.openDevice(selectedIface, CaptureSessionKFujii.DEFAULT_SNAPLEN, isPromiscuous(), DEFAULT_TIMEOUT);
+            captor.setFilter(getFilter(), true);
+            captor.loopPacket(CaptureSessionKFujii.DEFAULT_COUNT, this);
+
+        } catch (Exception e) {
+            throw new CaptureException("Failed starting capture session", e);
+        }
+    }
+
+    @Override
+    protected void stopCapture() throws CaptureException {
+        if (captor != null) {
+            captor.breakLoop();
+            captor.close();
+
+        } else {
+            throw new CaptureException("Captor is not running, cannot stop it.");
+        }
+    }
+
+    public void receivePacket(jpcap.packet.Packet jpcapPacket) {
+        try {
+            Packet packet = new Packet();
+
+            if (jpcapPacket instanceof IPPacket) {
+                IPPacket ipPacket = (IPPacket) jpcapPacket;
+                packet.setSrcIPAddr(ipPacket.src_ip.getHostAddress());
+                packet.setDstIPAddr(ipPacket.dst_ip.getHostAddress());
+                long seconds  = ipPacket.sec;
+                long microSec = ipPacket.usec;
+                long milliSec = (seconds * 1000) + (microSec / 1000);
+                packet.setDate(new Date(milliSec));
+                
+                if (jpcapPacket instanceof UDPPacket) {
+                    UDPPacket udpPacket = (UDPPacket) jpcapPacket;
+                    packet.setData(new String(udpPacket.data));
+                    packet.setSrcIPPort(udpPacket.src_port);
+                    packet.setDstIPPort(udpPacket.dst_port);
+
+                } else if (jpcapPacket instanceof TCPPacket) {
+                    TCPPacket tcpPacket = (TCPPacket) jpcapPacket;
+                    packet.setData(new String(tcpPacket.data));
+                    packet.setSrcIPPort(tcpPacket.src_port);
+                    packet.setDstIPPort(tcpPacket.dst_port);
+
+                } else {
+                    throw new CaptureException("Unknown packet transport type:\n"
+                            + jpcapPacket);
+                }
+            } else {
+                throw new CaptureException("Unknown packet: \n" + jpcapPacket);
+            }
+
+            getListener().onPacket(packet);
+
+        } catch (Exception e) {
+            logger.error("Fail processing packet", e);
+        }
+    }
+}
--- util/scripts/sipana_deploy.sh
+++ util/scripts/sipana_deploy.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
 export JBOSS_HOME=/home/mhack/app/jboss-4.2.1.GA
+export JBOSS_SERVER=sipana
+export SERVER_DEPLOY="${JBOSS_HOME}/server/${JBOSS_SERVER}/deploy/"
+export SERVER_LIB="${JBOSS_HOME}/server/${JBOSS_SERVER}/lib/"
 export SIPANA_CLIENT=/home/mhack/app/sipana-client
 
 cd `dirname $0`
 cd ../../
 
-mvn clean 
-mvn 
+case "$1" in 
+  server)
+    deploy_server();
+    ;;
+  client)
+    deploy_client();
+    ;;
+  all)
+    deploy_server();
+    deploy_client();
+    ;;
+  *)
+    echo "\nUsage: $0 {server|client|all}\n"
+esac
 
-# server
-cp --verbose sipana-server-ejb/target/sipana-server-ejb-1.0.jar $JBOSS_HOME/server/default/deploy/ 
-cp --verbose sipana-server-war/target/sipana-server-war-1.0.war $JBOSS_HOME/server/default/deploy/ 
-cp --verbose sipana-commons/target/sipana-commons-1.0.jar $JBOSS_HOME/server/default/lib/ 
-cp --verbose sipana-sipscenario/target/sipana-sipscenario-1.0.jar $JBOSS_HOME/server/default/lib/ 
-cp --verbose sipana-server-ear/src/main/resources/*.xml $JBOSS_HOME/server/default/deploy/
+deploy_server() {
+  cp --verbose sipana-server-ejb/target/sipana-server-ejb-1.0.jar $SERVER_DEPLOY 
+  cp --verbose sipana-server-war/target/sipana-server-war-1.0.war $SERVER_DEPLOY 
+  cp --verbose sipana-commons/target/sipana-commons-1.0.jar $SERVER_LIB 
+  cp --verbose sipana-sipscenario/target/sipana-sipscenario-1.0.jar $SERVER_LIB 
+  cp --verbose sipana-server-ear/src/main/resources/*.xml $SERVER_DEPLOY
+}
 
-# client
-cp --verbose sipana-commons/target/sipana-commons-1.0.jar $SIPANA_CLIENT/jar
-cp --verbose sipana-client/target/sipana-client-1.0.jar $SIPANA_CLIENT/jar
-cp --verbose sipana-client/conf/* $SIPANA_CLIENT/conf/
-cp --verbose sipana-client/conf/jvm $SIPANA_CLIENT/conf/jvm/
-cp --verbose sipana-client/bin $SIPANA_CLIENT/bin/
+deploy_client() {
+  cp --verbose sipana-commons/target/sipana-commons-1.0.jar $SIPANA_CLIENT/jar
+  cp --verbose sipana-client/target/sipana-client-1.0.jar $SIPANA_CLIENT/jar
+  cp --verbose sipana-client/conf/* $SIPANA_CLIENT/conf/
+  cp --verbose sipana-client/conf/jvm $SIPANA_CLIENT/conf/jvm/
+  cp --verbose sipana-client/bin $SIPANA_CLIENT/bin/
+}
 
