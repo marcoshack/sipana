f16b9484897528ecffb244a847bddebf7eec054f
marcoshack <marcoshack@1070b9c0-c02c-0410-8e95-a1ee7272c91a>
2008-12-23 15:09:55 +0000

    License information. Licence statement and full copy in doc/ directory.

--- sipana-client/src/main/java/org/sipana/client/capture/Packet.java
+++ sipana-client/src/main/java/org/sipana/client/capture/Packet.java
@@ -1,3 +1,18 @@
+/**
+ * This file is part of Sipana project <http://sipana.org/>
+ *
+ * Sipana is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * Sipana is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.sipana.client.capture;
 
 import java.util.Date;
@@ -6,7 +21,7 @@ import java.util.Date;
  * A transport independent packet representation to abstract underlying packet
  * object.
  * 
- * @author mhack
+ * @author Marcos Hack <marcoshack@gmail.com>
  */
 public class Packet {
     private String srcIPAddr;
--- sipana-client/src/main/java/org/sipana/client/capture/impl/CaptureManagerImpl.java
+++ sipana-client/src/main/java/org/sipana/client/capture/impl/CaptureManagerImpl.java
@@ -1,3 +1,18 @@
+/**
+ * This file is part of Sipana project <http://sipana.org/>
+ *
+ * Sipana is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * Sipana is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.sipana.client.capture.impl;
 
 import java.util.HashMap;
@@ -9,6 +24,10 @@ import org.sipana.client.capture.CaptureListener;
 import org.sipana.client.capture.CaptureManager;
 import org.sipana.client.capture.CaptureSession;
 
+/**
+ *
+ * @author Marcos Hack <marcoshack@gmail.com>
+ */
 public class CaptureManagerImpl implements CaptureManager {
 	private static CaptureManagerImpl instance;
 	private Map<String,CaptureSession> sessionList;
