8def0547c0d3aa7dab3a0910b1cc94012eff2577
marcoshack <marcoshack@1070b9c0-c02c-0410-8e95-a1ee7272c91a>
2009-01-17 05:13:53 +0000

    - QueryParam (GET parameters) added to SIPSessionWS.getSIPSessionList() method; - Added @Wrapper annotation to SIPSessionWS.getSIPSessionList() to return List<SIPSession> - "endTime" condition parameter of HQL query fixed in SIPSessionManagerBean.getSIPSession() method

--- sipana-server-ws/src/main/java/org/sipana/server/ws/impl/SIPSessionWSBean.java
+++ sipana-server-ws/src/main/java/org/sipana/server/ws/impl/SIPSessionWSBean.java
@@ -15,7 +15,9 @@
  */
 package org.sipana.server.ws.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.StringTokenizer;
 import org.sipana.server.ws.*;
 import org.apache.log4j.Logger;
 import org.sipana.protocol.sip.SIPRequest;
@@ -40,7 +42,7 @@ public class SIPSessionWSBean implements SIPSessionWS {
 
     public SIPSession getSIPSession(long sessionId) {
         logger.debug("Processing getSIPSession");
-        
+
         SIPSession session = sessionManager.getSIPSession(sessionId);
         breakSIPSessionRefCycle(session);
 
@@ -48,23 +50,38 @@ public class SIPSessionWSBean implements SIPSessionWS {
         return session;
     }
 
-    public List<SIPSession> getSIPSessionList() {
-        logger.debug("Processing getSIPSessionList");
-        
-        Long startTime = null;
-        Long endTime = null;
-        String method = null;
-        String fromUser = null;
-        String toUser = null;
-        String callId = null;
-        List<String> ipAddrList = null;
-        
-        List<SIPSession> sessionList = sessionManager.getSIPSessions(startTime, endTime, method, fromUser, toUser, callId, ipAddrList);
-        for (SIPSession s : sessionList) {
-            breakSIPSessionRefCycle(s);
+    public List<SIPSession> getSIPSessionList(
+            Long startTime,
+            Long endTime,
+            String method,
+            String fromUser,
+            String toUser,
+            String callId,
+            String strIpAddrList) {
+
+        if (logger.isDebugEnabled()) {
+            StringBuilder sb = new StringBuilder("Processing getSIPSessionList");
+            sb.append(": startTime = ").append(startTime);
+            sb.append(", endTime = ").append(endTime);
+            sb.append(", method = ").append(method);
+            sb.append(", fromUser = ").append(fromUser);
+            sb.append(", userUser = ").append(toUser);
+            sb.append(", callId = ").append(callId);
+            sb.append(", ipAddrList = ").append(strIpAddrList);
+            logger.debug(sb);
         }
-        
-        logger.debug("getSIPSessionList processed");
+
+        List<String> ipAddrList = createIpAddrList(strIpAddrList);
+
+        List<SIPSession> sessionList = sessionManager.getSIPSessions(startTime, endTime, method, fromUser, toUser, callId, ipAddrList);
+        breakSIPSessionRefCycle(sessionList);
+
+        if (logger.isDebugEnabled()) {
+            StringBuilder sb = new StringBuilder("getSIPSessionList processed. ");
+            sb.append(sessionList.size()).append(" item(s) found.");
+            logger.debug(sb);
+        }
+
         return sessionList;
     }
 
@@ -72,9 +89,15 @@ public class SIPSessionWSBean implements SIPSessionWS {
      * JAXB throws an exception if there is a circular reference between
      * objects, this method breaks SIPSession/SIPMessage circular references.
      *
-     * @param SIPSession
+     * @param List<SIPSession>
      * @author Marcos Hack <marcoshack@gmail.com>
      */
+    private void breakSIPSessionRefCycle(List<SIPSession> sessionList) {
+        for (SIPSession s : sessionList) {
+            breakSIPSessionRefCycle(s);
+        }
+    }
+
     private void breakSIPSessionRefCycle(SIPSession s) {
         for (SIPResponse res : s.getResponses()) {
             res.setSipSession(null);
@@ -82,5 +105,23 @@ public class SIPSessionWSBean implements SIPSessionWS {
         for (SIPRequest req : s.getRequests()) {
             req.setSipSession(null);
         }
+    }
+
+    private List<String> createIpAddrList(String strIPAddrList) {
+        List<String> addrList = null;
+
+        if (strIPAddrList != null) {
+            String csv = strIPAddrList.replaceAll(" +", ",");
+            StringTokenizer tokenizer = new StringTokenizer(csv, ",");
+
+            if (tokenizer.countTokens() > 0) {
+                addrList = new ArrayList<String>();
+                while (tokenizer.hasMoreTokens()) {
+                    addrList.add(tokenizer.nextToken());
+                }
+            }
+        }
+
+        return addrList;
     }
 }
